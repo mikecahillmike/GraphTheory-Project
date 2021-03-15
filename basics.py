c90d63fadf9c6b0a2f10ca6849fe5f0fc5773d89
mikecahillmike <mike@LAPTOP-K5QI7CQ0.localdomain>
2021-03-15 20:05:39 +0000

    Looked at basics

---
+++ basics.py
@@ -0,0 +1,16 @@
+# Michael Cahill
+# Python basics
+# 2021-03-15
+
+print("Hello World")
+
+a = 1
+b = 1.0
+s = "Hello, World from a string"
+t = '"Hello", from a different string'
+ 
+print(a, b , s, t)
+
+print(s[0:2]) # print char up to 2
+# String Slice
+print(s[3:10:2]) # print char starting at 3 and finishing at 10 but in every 2
