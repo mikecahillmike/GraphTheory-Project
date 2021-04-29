163d2ac4cabd4bbd4c79fdfee36190ad35d0d3c1
mikecahillmike <mike@LAPTOP-K5QI7CQ0.localdomain>
2021-04-29 23:02:56 +0100

    Working on fileInput

--- reg.py
+++ reg.py
@@ -14,16 +14,21 @@ with open('test.txt', 'r') as f:
         line_list = stripped_line.split()
         list_of_lists.append(line_list)
     print(list_of_lists)
-  
 
-    for test in list_of_lists:
-        infix = test[0]
-        print(f"infix:  {infix}")
-        postfix = shuntingre.shunt(infix)
-        print(f"postfix: {postfix}")
-        nfa = thompson.re_to_nfa(postfix)
-        print(f"thompson: {nfa}")
-        for s in test[1]:
-            match = nfa.match(s)
-            print(f"Match '{s}': {match}")
-        print()
+
+for test in list_of_lists:
+    infix = test[0]
+    print(f"infix:  {infix}")
+    postfix = shuntingre.shunt(infix)
+    print(f"postfix: {postfix}")
+    nfa = thompson.re_to_nfa(postfix)
+    print(f"thompson: {nfa}")
+    match = nfa.match(test[1])
+    print(f"Match '{test[1]}': {match}")
+    match = nfa.match(test[2])
+    print(f"Match '{test[2]}': {match}")
+    match = nfa.match(test[3])
+    print(f"Match '{test[3]}': {match}")
+    match = nfa.match(test[4])
+    print(f"Match '{test[4]}': {match}")
+    print()
