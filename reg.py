e1c4047d6e991efe7adb063d6436d57d8838d95c
mikecahillmike <mike@LAPTOP-K5QI7CQ0.localdomain>
2021-04-29 22:54:07 +0100

    File reading1

--- reg.py
+++ reg.py
@@ -1,43 +1,29 @@
 import shuntingre
 import thompson
 
-box[0] =[ ["(a.b|b*)",    ["ab", "b", "bb", "a"]]
-
-#if __name__ == "__main__":
-#    tests = [   ["(a.b|b*)",    ["ab", "b", "bb", "a"]]
-#            ,   ["a.(b.b)*.a",  ["aa", "abba", "aba"]]
-#            ,   ["1.(0.0)*.1",  ["11", "100001", "11001"]] 
-#    ]
+if __name__ == "__main__":
+    tests = [   ["(a.b|b*)",  ["ab", "b", "bb", "a"]]
+    ]
 
 
-with open('file.txt', 'r') as f:
-    lines = []
-    lines += f.readline()
-    print(lines[-1])
+with open('test.txt', 'r') as f:
+    #words =  set(open('test.txt').read().split())
+    list_of_lists = []
+    for line in f:
+        stripped_line = line.strip()
+        line_list = stripped_line.split()
+        list_of_lists.append(line_list)
+    print(list_of_lists)
+  
 
-f.readline()[0]
-box [1]= f 
-infix = box[1]
-print(f"infix:  {infix}")
-postfix = shuntingre.shunt(infix)
-print(f"postfix: {postfix}")
-nfa = thompson.re_to_nfa(postfix)
-print(f"thompson: {nfa}")
-
-
-# with open('file.txt, 'r') as f:
-    # Read a line of f.
-    # See if it matches the regular expression
-    # if it does print the line
-
-#for test in tests:
-#    infix = test[0]
-#    print(f"infix:  {infix}")
-#    postfix = shuntingre.shunt(infix)
-#    print(f"postfix: {postfix}")
-#    nfa = thompson.re_to_nfa(postfix)
-#    print(f"thompson: {nfa}")
-#    for s in test[1]:
-#        match = nfa.match(s)
-#        print(f"Match '{s}': {match}")
-#    print()
+    for test in list_of_lists:
+        infix = test[0]
+        print(f"infix:  {infix}")
+        postfix = shuntingre.shunt(infix)
+        print(f"postfix: {postfix}")
+        nfa = thompson.re_to_nfa(postfix)
+        print(f"thompson: {nfa}")
+        for s in test[1]:
+            match = nfa.match(s)
+            print(f"Match '{s}': {match}")
+        print()
